a711974e414f74955334146910634fbfb24f6a60
posutsai <posutsai@gmail.com>
2020-06-30 20:07:53 +0800

    lavamore baseline5 model seems working

--- transformer.py
+++ transformer.py
@@ -15,7 +15,7 @@ N_GRAM = 1
 BATCH_SIZE = 128
 EPOCH = 200
 PiRNA_L = 21
-MRNA_L = 31
+MRNA_L = 37
 def preprocess(pos_path, neg_path):
     pos_data = {"mRNA": [], "piRNA": []}
     neg_data = {"mRNA": [], "piRNA": []}
@@ -37,21 +37,117 @@ def preprocess(pos_path, neg_path):
     return pos_data, neg_data
 
 class FELayer(nn.Module):
-	def __init__(self, layer_infos, last_norm=True, norm_type='batch', bias=True):
-		super(FELayer, self).__init__()
-
-		self.linear_layers = nn.Sequential()
-		for idx, li in enumerate(layer_infos):
-			self.linear_layers.add_module(f'linear_{idx}', nn.Linear(li[0], li[1], bias=bias))
-			if idx == len(last_infos) - 1 and not last_norm:
-				break
-			self.linear_layers.add_module(f'bn_{idx}', nn.LayerNorm(li[1]) if norm_type != 'batch' else nn.BatchNorm1d(li[1]))
-			self.linear_layers.add_module(f'relu_{idx}', nn.PReLU())
-			if len(li) == 3:
-				self.linear_layers.add_module(f'dropout_{idx}', nn.Dropout(li[2]))
-
+    def __init__(self, layer_infos, last_norm=True, norm_type='batch', bias=True):
+        super(FELayer, self).__init__()
+        self.linear_layers = nn.Sequential()
+        for idx, li in enumerate(layer_infos):
+            self.linear_layers.add_module(f'linear_{idx}', nn.Linear(li[0], li[1], bias=bias))
+            if idx == len(layer_infos) - 1 and not last_norm:
+                break
+            self.linear_layers.add_module(f'bn_{idx}', nn.LayerNorm(li[1]) if norm_type != 'batch' else nn.BatchNorm1d(li[1]))
+            self.linear_layers.add_module(f'relu_{idx}', nn.PReLU())
+            if len(li) == 3:
+                self.linear_layers.add_module(f'dropout_{idx}', nn.Dropout(li[2]))
     def forward(self, x):
         return self.linear_layers(x)
+
+class SEblock(nn.Module):
+    def __init__(self, channels, reduction=4):
+        super(SEblock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.fc1 = nn.Linear(channels, channels//reduction)
+        self.relu = nn.PReLU()#(inplace=True)
+        self.fc2 = nn.Linear(channels//reduction, channels)
+        self.sigmoid = nn.Sigmoid()
+
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        torch.nn.init.zeros_(self.fc1.bias)
+
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+        torch.nn.init.zeros_(self.fc2.bias)
+
+    def forward(self, x):
+        input_x = x
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        x = x.view(x.size(0), x.size(1), 1)
+
+        return input_x * x
+
+class SELayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, reduction=4, add_residual=False, res_dim=16):
+        super(SELayer, self).__init__()
+        self.conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
+                              kernel_size=kernel_size, stride=stride, padding=(kernel_size//2))
+        self.bn1 = nn.BatchNorm1d(out_channels)
+        self.relu = nn.PReLU()#(inplace=True)
+        self.se = SEblock(channels=out_channels, reduction=reduction)
+        if add_residual:
+            self.conv2 = nn.Conv1d(in_channels=res_dim, out_channels=out_channels, kernel_size=1)
+            self.bn2 = nn.BatchNorm1d(out_channels)
+
+        torch.nn.init.xavier_uniform_(self.conv.weight)
+        torch.nn.init.zeros_(self.conv.bias)
+
+    def forward(self, x, residual=None):
+        x = self.conv(x)
+        x = self.bn1(x)
+        if residual is not None:
+            x = x + self.bn1(self.conv2(residual))
+        x = self.relu(x)
+        x = self.se(x)
+
+        return x
+
+class BaseLine5(nn.Module):
+    def __init__(self, emb_dim=4, rnn_dim=20, upsample_rate=2, bidirect=True, rnn_layer=1, cnn_dim=16, mrna_len=31, pirna_len=21):
+        super(BaseLine5, self).__init__()
+        self.mrna_len = mrna_len
+        self.pirna_len = pirna_len
+        self.mrna_conv = SELayer(emb_dim, cnn_dim, 5, stride=1, reduction=4)
+        self.pirna_conv = SELayer(emb_dim, cnn_dim, 5, stride=1, reduction=4)
+        self.mrna_fe = FELayer([
+            [mrna_len, mrna_len//2, 0.3],
+            [mrna_len//2, mrna_len, 0.3],
+        ], last_norm=False, norm_type='layer')
+        self.pirna_fe = FELayer([
+            [pirna_len, pirna_len//2, 0.3],
+            [pirna_len//2, mrna_len, 0.3],
+        ], last_norm=False, norm_type='layer')
+        self.rnn_dim = rnn_dim
+        self.rnn_layer = rnn_layer
+        self.bidirect = bidirect
+        self.rnn_direction = 2 if self.bidirect else 1
+        self.rnn = nn.LSTM(cnn_dim*2, rnn_dim, bidirectional=self.bidirect, num_layers=self.rnn_layer, dropout=0.5)
+
+        self.cls_input_dim = mrna_len*rnn_dim*self.rnn_direction
+        self.cls_layer = FELayer([
+            [self.cls_input_dim, self.cls_input_dim//4, 0.5],
+            [self.cls_input_dim//4, self.cls_input_dim//16, 0.5],
+            [self.cls_input_dim//16, 2]
+        ], last_norm=False, norm_type='batch')
+
+    def _init_hidden_(self):
+        return (
+            torch.randn(self.rnn_layer*self.rnn_direction, BATCH_SIZE, self.rnn_dim).to(dev),
+            torch.randn(self.rnn_layer*self.rnn_direction, BATCH_SIZE, self.rnn_dim).to(dev)
+        )
+    def forward(self, mrna_x, pirna_x):
+        mrna_out = self.mrna_conv(mrna_x)
+        mrna_out = self.mrna_fe(mrna_out)
+
+        pirna_out = self.pirna_conv(pirna_x)
+        pirna_out = self.pirna_fe(pirna_out)
+        out = torch.cat((mrna_out, pirna_out), dim=1).permute(2, 0, 1)
+        out, (h_n, c_n) = self.rnn(out, self._init_hidden_())
+        out = out.permute(1, 2, 0)
+        out = out.reshape(out.shape[0], -1)
+        out = self.cls_layer(out)
+        return out
 
 def ngram_encode(seq, mode="overlap"):
 
@@ -78,44 +174,6 @@ def ngram_encode(seq, mode="overlap"):
             result.append(lut[key])
     return result
 
-class MPiNet(nn.Module):
-    def __init__(self, is_window_mask=True):
-        super(MPiNet, self).__init__()
-        self.transformer = nn.Transformer(
-            d_model=4**N_GRAM,
-            nhead=4,
-            num_encoder_layers=6,
-            num_decoder_layers=6,
-            # dropout=0.2
-        )
-        self.linear1 = nn.Linear(4, 1)
-        self.linear2 = nn.Linear(21, 2)
-        if is_window_mask:
-            self.tgt_mask = self.piRNA_mask_gen(PiRNA_L, 3)
-        else:
-            self.tgt_mask = self.transformer.generate_square_subsequent_mask(PiRNA_L)
-        self.tgt_mask = self.tgt_mask.to(dev)
-        # self.tgt_mask.requires_grad = False
-
-    def piRNA_mask_gen(self, seq_l, unmasked):
-        mask = (torch.triu(torch.ones(seq_l, seq_l)) == 1).transpose(0, 1)
-        submask = torch.triu(
-            torch.ones(seq_l - unmasked, seq_l - unmasked) - torch.eye(seq_l - unmasked)
-        ) == 1
-        mask[unmasked:, :(seq_l - unmasked)] = submask
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
-
-    def forward(self, m, pi):
-        pi = pi.permute(2, 0, 1)
-        m = m.permute(2, 0, 1)
-        w = self.transformer(m, pi, tgt_mask=self.tgt_mask)
-        w = w.permute(1, 0, 2)
-        w = nn.functional.relu(self.linear1(w), 2).permute(0, 2, 1)
-        w = torch.flatten(w, start_dim=1)
-        w = self.linear2(w)
-        return w
-
 def get_next_batch(m, pi, batch_size, train_i):
     batch = 0
     train_size = train_i.shape[0]
@@ -139,10 +197,10 @@ def train(pos_data, neg_data):
     np.random.shuffle(index)
     valid_i = index[:128]
     train_i = index[2000:]
-    model = MPiNet().to(dev)
+    model = BaseLine5().to(dev)
     cse = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    Total_params = sum(p.numel() for p in model.parameters())
+    total_params = sum(p.numel() for p in model.parameters())
     print(f"total number of trainable {total_params}")
     for e in range(EPOCH):
         model.train()
