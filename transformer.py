37b1f34275cce6fee0a5af863f8852d85a9c763c
posutsai <posutsai@gmail.com>
2020-07-01 03:12:38 +0800

    Integrate transformer into architecture

--- transformer.py
+++ transformer.py
@@ -15,7 +15,7 @@ N_GRAM = 1
 BATCH_SIZE = 128
 EPOCH = 200
 PiRNA_L = 21
-MRNA_L = 37
+MRNA_L = 31
 def preprocess(pos_path, neg_path):
     pos_data = {"mRNA": [], "piRNA": []}
     neg_data = {"mRNA": [], "piRNA": []}
@@ -84,7 +84,7 @@ class SELayer(nn.Module):
         self.conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
                               kernel_size=kernel_size, stride=stride, padding=(kernel_size//2))
         self.bn1 = nn.BatchNorm1d(out_channels)
-        self.relu = nn.PReLU()#(inplace=True)
+        self.relu = nn.ReLU()#(inplace=True)
         self.se = SEblock(channels=out_channels, reduction=reduction)
         if add_residual:
             self.conv2 = nn.Conv1d(in_channels=res_dim, out_channels=out_channels, kernel_size=1)
@@ -103,32 +103,23 @@ class SELayer(nn.Module):
 
         return x
 
-class BaseLine5(nn.Module):
-    def __init__(self, emb_dim=4, rnn_dim=20, upsample_rate=2, bidirect=True, rnn_layer=1, cnn_dim=16, mrna_len=31, pirna_len=21):
-        super(BaseLine5, self).__init__()
-        self.mrna_len = mrna_len
-        self.pirna_len = pirna_len
-        self.mrna_conv = SELayer(emb_dim, cnn_dim, 5, stride=1, reduction=4)
+class MPiNet(nn.Module):
+    def __init__(self, emb_dim=4, upsample_rate=2, cnn_dim=16):
+        super(MPiNet, self).__init__()
         self.pirna_conv = SELayer(emb_dim, cnn_dim, 5, stride=1, reduction=4)
-        self.mrna_fe = FELayer([
-            [mrna_len, mrna_len//2, 0.3],
-            [mrna_len//2, mrna_len, 0.3],
-        ], last_norm=False, norm_type='layer')
         self.pirna_fe = FELayer([
-            [pirna_len, pirna_len//2, 0.3],
-            [pirna_len//2, mrna_len, 0.3],
+            [PiRNA_L, PiRNA_L//2, 0.3],
+            [PiRNA_L//2, MRNA_L, 0.3],
         ], last_norm=False, norm_type='layer')
-        self.rnn_dim = rnn_dim
-        self.rnn_layer = rnn_layer
-        self.bidirect = bidirect
-        self.rnn_direction = 2 if self.bidirect else 1
-        self.rnn = nn.LSTM(cnn_dim*2, rnn_dim, bidirectional=self.bidirect, num_layers=self.rnn_layer, dropout=0.5)
+        self.cls_conv = SELayer(cnn_dim + 4, 4, 5, stride=1, reduction=4)
 
-        self.cls_input_dim = mrna_len*rnn_dim*self.rnn_direction
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=20, nhead=5, dropout=0.15)
+        self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=4)
+
+        self.cls_input_dim = MRNA_L * 4
         self.cls_layer = FELayer([
-            [self.cls_input_dim, self.cls_input_dim//4, 0.5],
-            [self.cls_input_dim//4, self.cls_input_dim//16, 0.5],
-            [self.cls_input_dim//16, 2]
+            [self.cls_input_dim, self.cls_input_dim//4, 0.3],
+            [self.cls_input_dim//4, 2]
         ], last_norm=False, norm_type='batch')
 
     def _init_hidden_(self):
@@ -137,15 +128,14 @@ class BaseLine5(nn.Module):
             torch.randn(self.rnn_layer*self.rnn_direction, BATCH_SIZE, self.rnn_dim).to(dev)
         )
     def forward(self, mrna_x, pirna_x):
-        mrna_out = self.mrna_conv(mrna_x)
-        mrna_out = self.mrna_fe(mrna_out)
-
+        # Originally shape (Batch_size, dim, length)
+        # mrna_x = mrna_x.permute(2, 0, 1) (len, batch_size, dim)
         pirna_out = self.pirna_conv(pirna_x)
         pirna_out = self.pirna_fe(pirna_out)
-        out = torch.cat((mrna_out, pirna_out), dim=1).permute(2, 0, 1)
-        out, (h_n, c_n) = self.rnn(out, self._init_hidden_())
-        out = out.permute(1, 2, 0)
-        out = out.reshape(out.shape[0], -1)
+        out = torch.cat((mrna_x, pirna_out), dim=1).permute(2, 0, 1)
+        encoded = self.encoder(out).permute(1, 2, 0)
+        out = self.cls_conv(encoded)
+        out = torch.reshape(out, (BATCH_SIZE, self.cls_input_dim))
         out = self.cls_layer(out)
         return out
 
@@ -195,9 +185,10 @@ def train(pos_data, neg_data):
     pi = np.concatenate((pos_data["piRNA"], neg_data["piRNA"]), axis=0)
     index = np.arange(answers.shape[0])
     np.random.shuffle(index)
-    valid_i = index[:128]
-    train_i = index[2000:]
-    model = BaseLine5().to(dev)
+    ten_fold = answers.shape[0] // 10
+    valid_i = index[:ten_fold]
+    train_i = index[ten_fold:]
+    model = MPiNet().to(dev)
     cse = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     total_params = sum(p.numel() for p in model.parameters())
@@ -205,30 +196,34 @@ def train(pos_data, neg_data):
     for e in range(EPOCH):
         model.train()
         train_acc = 0.
-        b = 0
+        train_b = 0
         for b_m, b_pi, bi in get_next_batch(m, pi, BATCH_SIZE, train_i):
             y_ = model(b_m, b_pi)
             predict = torch.argmax(torch.nn.functional.softmax(y_, 1), 1)
             predict = predict.cpu().detach().numpy()
-            loss = cse(y_, torch.tensor(answers[bi]).to(dev).long())
+            train_loss = cse(y_, torch.tensor(answers[bi]).to(dev).long())
             train_acc += metrics.accuracy_score(answers[bi], predict)
-            loss.backward()
+            train_loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            b += 1
-            if b * BATCH_SIZE > train_i.shape[0]:
-                model.eval()
-                y_ = model(
-                    torch.tensor(m[valid_i]).to(dev).float(),
-                    torch.tensor(pi[valid_i]).to(dev).float()
-                )
-                predict = torch.argmax(torch.nn.functional.softmax(y_, 1), 1)
-                predict = predict.cpu().detach().numpy()
-                valid_loss = cse(y_, torch.tensor(answers[valid_i]).to(dev).long())
-                valid_acc = metrics.accuracy_score(answers[valid_i], predict)
-
-                print(f"train_acc is {train_acc / b: .3f}, loss is {loss: .3f}; valid_acc is {valid_acc:.3f}, valid_loss is {valid_loss:.3f}")
+            train_b += 1
+            if train_b * BATCH_SIZE > train_i.shape[0]:
                 break
+        model.eval()
+        valid_acc = 0.
+        valid_b = 0
+        for v_m, v_pi, vi in get_next_batch(m, pi, BATCH_SIZE, valid_i):
+            y_ = model(v_m, v_pi)
+            predict = torch.argmax(torch.nn.functional.softmax(y_, 1), 1)
+            predict = predict.cpu().detach().numpy()
+            valid_loss = cse(y_, torch.tensor(answers[vi]).to(dev).long())
+            valid_acc += metrics.accuracy_score(answers[vi], predict)
+            valid_b += 1
+            if valid_b * BATCH_SIZE > valid_i.shape[0]:
+                break
+        print(f"#{e:3d} th ", end=" ")
+        print(f"train_acc is {train_acc / train_b: .3f}, train_loss is {train_loss: .3f}", end="; ")
+        print(f"valid_acc is {valid_acc / valid_b: .3f}, valid_loss is {valid_loss: .3f}")
 
 if __name__ == "__main__":
     with open("save_time/processed_dataset.npy", "rb") as f:
